0d68a34d35ce44e54c8fc7ddcd249a8fc986cafe
Hannah Stahl <hls62@cornell.edu>
2020-02-03 11:37:33 -0600

    Created basic layout based on template repo

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Home = () => (
+  <div>
+    <h1>Make your mark without leaving a mark.</h1>
+    <Button>Shop Products</Button>
+  </div>
+);
+
+export default Home;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Portfolio = () => (
+  <div>
+    <h1>Products Page</h1>
+    <p>Insert products here.</p>
+  </div>
+);
+
+export default Portfolio;
