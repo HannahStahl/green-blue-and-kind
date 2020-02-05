ccdeb300e788a8fa84eab3279f6623a2dd6bbfe4
Hannah Stahl <hls62@cornell.edu>
2020-02-05 13:23:35 -0600

    Added filters that currently don't actually filter anything

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ItemsList from './ItemsList';
 import config from '../config';
+import Filter from './Filter';
 
-const Category = (props) => {
+export default function Category(props) {
   const [products, setProducts] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [selectedTagIds, setSelectedTagIds] = useState([]);
+
   useEffect(() => {
     const promises = [
       fetch(`${config.apiURL}/products/${props.match.params.id}`).then(res => res.json()),
       fetch(`${config.apiURL}/productsToPhotos`).then(res => res.json()),
       fetch(`${config.apiURL}/photos`).then(res => res.json()),
+      fetch(`${config.apiURL}/tags`).then(res => res.json()),
     ];
     Promise.all(promises).then((results) => {
-      const products = results[0];
-      const productsToPhotos = results[1];
-      const photos = results[2];
+      const [products, productsToPhotos, photos, tags] = results;
       products.forEach((product, index) => {
         const photoIds = productsToPhotos
           .filter(productToPhoto => productToPhoto.productId === product.productId)
@@ -21,10 +24,23 @@ const Category = (props) => {
         products[index].productPhotos = photos.filter(photo => photoIds.includes(photo.photoId));
       });
       setProducts(products);
+      setTags(tags);
     });
   }, [props.match.params.id]);
+
+  function toggleTag(tagId) {
+    const index = selectedTagIds.indexOf(tagId);
+    if (index > -1) {
+      selectedTagIds.splice(index, 1);
+    } else {
+      selectedTagIds.push(tagId);
+    }
+    setSelectedTagIds([...selectedTagIds]);
+  }
+
   return (
-    <div className="page-content">
+    <div className="page-content category-page">
+      <Filter tags={tags} selectedIds={selectedTagIds} toggleTag={toggleTag} />
       <ItemsList alignment="center" items={products ? products.map(product => ({
         id: product.productId,
         name: product.productName,
@@ -34,5 +50,3 @@ const Category = (props) => {
     </div>
   );
 };
-
-export default Category;
---
+++ src/components/Filter.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export default function Filter({ tags, selectedIds, toggleTag }) {
+  const [selectedTagIds, setSelectedTagIds] = useState(selectedIds);
+  useEffect(() => {
+    setSelectedTagIds(selectedIds);
+  }, [selectedIds]);
+  return (
+    <span className="filter">
+      <span className="filter-label"><p>Filter:</p></span>
+      <span className="tags">
+        {tags.map((tag) => {
+          const selected = selectedTagIds.includes(tag.tagId);
+          const className = `tag${selected ? ' selected' : ''}`;
+          return (
+            <span
+              key={tag.tagId}
+              className={className}
+              onClick={() => toggleTag(tag.tagId)}
+            >
+              <p>{tag.tagName}</p>
+            </span>
+          );
+        })}
+      </span>
+    </span>
+  );
+};
