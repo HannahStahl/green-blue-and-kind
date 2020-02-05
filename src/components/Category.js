66f4fb31da54d77a174f7d391d12d06aa05661fc
Hannah Stahl <hls62@cornell.edu>
2020-02-05 14:18:13 -0600

    Implemented filter functionality

--- src/components/Category.js
+++ src/components/Category.js
@@ -7,24 +7,31 @@ export default function Category(props) {
   const [products, setProducts] = useState([]);
   const [tags, setTags] = useState([]);
   const [selectedTagIds, setSelectedTagIds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const promises = [
       fetch(`${config.apiURL}/products/${props.match.params.id}`).then(res => res.json()),
       fetch(`${config.apiURL}/productsToPhotos`).then(res => res.json()),
       fetch(`${config.apiURL}/photos`).then(res => res.json()),
+      fetch(`${config.apiURL}/productsToTags`).then(res => res.json()),
       fetch(`${config.apiURL}/tags`).then(res => res.json()),
     ];
     Promise.all(promises).then((results) => {
-      const [products, productsToPhotos, photos, tags] = results;
+      const [products, productsToPhotos, photos, productsToTags, tags] = results;
       products.forEach((product, index) => {
         const photoIds = productsToPhotos
           .filter(productToPhoto => productToPhoto.productId === product.productId)
           .map(productToPhoto => productToPhoto.photoId);
         products[index].productPhotos = photos.filter(photo => photoIds.includes(photo.photoId));
+        const tagIds = productsToTags
+          .filter(productToTag => productToTag.productId === product.productId)
+          .map(productToTag => productToTag.tagId);
+        products[index].productTagIds = tags.map(tag => tag.tagId).filter(tagId => tagIds.includes(tagId));
       });
       setProducts(products);
       setTags(tags);
+      setLoading(false);
     });
   }, [props.match.params.id]);
 
@@ -38,15 +45,35 @@ export default function Category(props) {
     setSelectedTagIds([...selectedTagIds]);
   }
 
-  return (
+  function shouldDisplayProduct(product) {
+    if (selectedTagIds.length === 0) {
+      return true;
+    }
+    let selectedTagIdFound = false;
+    let i = 0;
+    while (i < product.productTagIds.length && !selectedTagIdFound) {
+      const productTagId = product.productTagIds[i];
+      if (selectedTagIds.includes(productTagId)) {
+        selectedTagIdFound = true;
+      }
+      i += 1;
+    }
+    return selectedTagIdFound;
+  }
+
+  const productsToDisplay = products ? products.filter(shouldDisplayProduct).map(product => ({
+    id: product.productId,
+    name: product.productName,
+    url: `/products/${product.productId}`,
+    photo: product.productPhotos[0].photoName,
+  })) : [];
+
+  return !loading && (
     <div className="page-content category-page">
       <Filter tags={tags} selectedIds={selectedTagIds} toggleTag={toggleTag} />
-      <ItemsList alignment="center" items={products ? products.map(product => ({
-        id: product.productId,
-        name: product.productName,
-        url: `/products/${product.productId}`,
-        photo: product.productPhotos[0].photoName,
-      })) : []} />
+      {productsToDisplay.length > 0 ? (
+        <ItemsList alignment="center" items={productsToDisplay} />
+      ) : <div className="no-items"><p>No products with the selected filters.</p></div>}
     </div>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Product = (props) => {
+const Product = () => {
   return (
     <div className="page-content">
       TODO
