5309668305c004562e4997fb04514f4be8b2a20f
Hannah Stahl <hls62@cornell.edu>
2020-02-05 16:10:54 -0600

    Added size, color, and quantity selectors to product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import config from '../config';
 
 export default function Product(props) {
   const [product, setProduct] = useState({});
+  const [size, setSize] = useState(null);
+  const [color, setColor] = useState(null);
+  const [quantity, setQuantity] = useState(null);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const productId = props.match.params.id;
     const promises = [
@@ -21,32 +28,48 @@ export default function Product(props) {
       const [
         product, productsToPhotos, photos, productsToTags, tags, productsToSizes, sizes, productsToColors, colors,
       ] = results;
-      const photoIds = productsToPhotos
+      const productPhotoIds = productsToPhotos
         .filter(productToPhoto => productToPhoto.productId === product.productId)
         .map(productToPhoto => productToPhoto.photoId);
-      product.productPhotos = photos.filter(photo => photoIds.includes(photo.photoId));
-      const tagIds = productsToTags
+      product.productPhotos = photos.filter(photo => productPhotoIds.includes(photo.photoId));
+      const productTagIds = productsToTags
         .filter(productToTag => productToTag.productId === product.productId)
         .map(productToTag => productToTag.tagId);
-      product.productTagIds = tags.map(tag => tag.tagId).filter(tagId => tagIds.includes(tagId));
-      const sizeIds = productsToSizes
+      product.productTags = tags.filter(tag => productTagIds.includes(tag.tagId)).map(tag => tag.tagName);
+      const productSizeIds = productsToSizes
         .filter(productToSize => productToSize.productId === product.productId)
         .map(productToSize => productToSize.sizeId);
-      product.productSizeIds = sizes.map(size => size.sizeId).filter(sizeId => sizeIds.includes(sizeId));
-      const colorIds = productsToColors
+      product.productSizes = sizes.filter(size => productSizeIds.includes(size.sizeId));
+      const productColorIds = productsToColors
         .filter(productToColor => productToColor.productId === product.productId)
         .map(productToColor => productToColor.colorId);
-      product.productColorIds = colors.map(color => color.colorId).filter(colorId => colorIds.includes(colorId));
+      product.productColors = colors.filter(color => productColorIds.includes(color.colorId));
       setProduct(product);
       setLoading(false);
     });
   }, [props.match.params.id]);
+
+  function validateForm() {
+    return size && color && quantity;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('here');
+    // TODO add to cart then clear inputs and show 
+  }
+
   return !loading && (
     <div className="page-content product-page">
       <div className="product-info">
         <div className="product-photos">
           {product.productPhotos.map(photo => (
-            <img src={`${config.cloudfrontURL}/${photo.photoName}`} alt={photo.photoName} className="product-photo" />
+            <img
+              key={photo.photoId}
+              src={`${config.cloudfrontURL}/${photo.photoName}`}
+              alt={photo.photoName}
+              className="product-photo"
+            />
           ))}
         </div>
         <div className="product-details">
@@ -59,7 +82,56 @@ export default function Product(props) {
               </p>
             ) : <p>{`$${product.productPrice}`}</p>}
           </div>
-          <p>{product.productDescription}</p>
+          <p className="product-description">{product.productDescription}</p>
+          {product.productTags.length > 0 && (
+            <div className="product-tags">
+              {product.productTags.map(tag => <div key={tag} className="product-tag">{tag}</div>)}
+            </div>
+          )}
+          <form onSubmit={handleSubmit} className="product-form">
+            <FormGroup controlId="size">
+              <FormControl
+                as="select"
+                value={size || ""}
+                onChange={e => setSize(e.target.value)}
+              >
+                <option key="" value="" disabled>Size</option>
+                {product.productSizes.map(productSize => (
+                  <option key={productSize.sizeId} value={productSize.sizeId}>{productSize.sizeName}</option>
+                ))}
+              </FormControl>
+            </FormGroup>
+            <FormGroup controlId="color">
+              <FormControl
+                as="select"
+                value={color || ""}
+                onChange={e => setColor(e.target.value)}
+              >
+                <option key="" value="" disabled>Color</option>
+                {product.productColors.map(productColor => (
+                  <option key={productColor.colorId} value={productColor.colorId}>{productColor.colorName}</option>
+                ))}
+              </FormControl>
+            </FormGroup>
+            <FormGroup controlId="quantity">
+              <FormControl
+                type="number"
+                min="0"
+                step="1"
+                value={quantity || 0}
+                onChange={e => setQuantity(e.target.value)}
+              />
+            </FormGroup>
+            <Button
+              block
+              type="submit"
+              size="lg"
+              variant="outline-primary"
+              disabled={!validateForm()}
+            >
+              Add to Cart
+            </Button>
+          </form>
         </div>
       </div>
     </div>
