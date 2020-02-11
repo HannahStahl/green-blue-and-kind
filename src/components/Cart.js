d57314075bde10c97457facb448c92c51a62fd27
Hannah Stahl <hls62@cornell.edu>
2020-02-10 18:44:43 -0600

    Added Estimated Total, email address input, and Submit Request button to cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import FormGroup from 'react-bootstrap/FormGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import config from '../config';
 
 export default function Cart() {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     let cart = localStorage.getItem('cart');
@@ -65,50 +68,102 @@ export default function Cart() {
     setItems([...items]);
   }
 
+  function getTotal() {
+    let total = 0;
+    items.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total;
+  }
+
+  function validateForm() {
+    return email.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // TODO send email to Shana (you, on dev)
+  }
+
   return !loading && (
     <div className="page-content cart-page">
       {items.length > 0 ? (
-        <table className="cart-items">
-          <tbody>
-            <tr key="cart-header" className="cart-item cart-header">
-              <td />
-              <td>Product</td>
-              <td>Size</td>
-              <td>Color</td>
-              <td>Quantity</td>
-              <td>Price</td>
-              <td />
-            </tr>
-            {items.map((item, index) => (
-              <tr key={`${item.productId}-${item.sizeId}-${item.colorId}`} className="cart-item">
-                <td>
-                  <a href={`/products/${item.productId}`}>
-                    <img src={`${config.cloudfrontURL}/${item.photoName}`} alt={item.productName} />
-                  </a>
-                </td>
-                <td>
-                  <a href={`/products/${item.productId}`} className="product-name">
-                    <p>{item.productName}</p>
-                  </a>
-                </td>
-                <td><p>{item.sizeName}</p></td>
-                <td><p>{item.colorName}</p></td>
-                <td>
-                  <FormControl
-                    type="number"
-                    min="0"
-                    step="1"
-                    value={item.quantity}
-                    onChange={(e) => updateQuantity(e.target.value, index)}
-                    className="quantity"
-                  />
-                </td>
-                <td><p>${item.quantity * item.price}</p></td>
-                <td><p><i className="fas fa-times" onClick={() => removeItem(index)} /></p></td>
+        <div>
+          <table className="cart-items">
+            <tbody>
+              <tr key="cart-header" className="cart-item cart-header">
+                <td />
+                <td />
+                <td>Product</td>
+                <td>Size</td>
+                <td>Color</td>
+                <td>Quantity</td>
+                <td>Price</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
+              {items.map((item, index) => (
+                <tr key={`${item.productId}-${item.sizeId}-${item.colorId}`} className="cart-item">
+                  <td><p><i className="fas fa-times" onClick={() => removeItem(index)} /></p></td>
+                  <td>
+                    <a href={`/products/${item.productId}`}>
+                      <img src={`${config.cloudfrontURL}/${item.photoName}`} alt={item.productName} />
+                    </a>
+                  </td>
+                  <td>
+                    <a href={`/products/${item.productId}`} className="product-name">
+                      <p>{item.productName}</p>
+                    </a>
+                  </td>
+                  <td><p>{item.sizeName}</p></td>
+                  <td><p>{item.colorName}</p></td>
+                  <td>
+                    <FormControl
+                      type="number"
+                      min="0"
+                      step="1"
+                      value={item.quantity}
+                      onChange={(e) => updateQuantity(e.target.value, index)}
+                      className="quantity"
+                    />
+                  </td>
+                  <td><p className="price">${item.quantity * item.price}</p></td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="7">
+                  <div className="cart-total-container">
+                    <p>Estimated Total:</p>
+                    <p className="cart-total">${getTotal()}</p>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="7">
+                  <div className="submit-request">
+                    <form onSubmit={handleSubmit}>
+                      <FormGroup controlId="email" className="email-container">
+                        <FormControl
+                          className="email"
+                          placeholder="Your Email Address"
+                          type="email"
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
+                        />
+                      </FormGroup>
+                      <Button
+                        type="submit"
+                        size="lg"
+                        variant="outline-primary"
+                        disabled={!validateForm()}
+                      >
+                        Submit Request
+                      </Button>
+                    </form>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       ) : <p className="no-items">No items in cart.</p>
     }
     </div>
