2a9b40d32c428b767324e5ede8b5caac0e64a2b6
Hannah Stahl <hls62@cornell.edu>
2020-02-16 18:59:24 -0600

    Updated CloudFront URLs to ones that point to public folder

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const dev = {
   apiURL: 'https://qwppkge4a7.execute-api.us-east-1.amazonaws.com/dev',
-  cloudfrontURL: 'https://d3lgv4qt5prjed.cloudfront.net',
+  cloudfrontURL: 'https://d1emv4i9ahobax.cloudfront.net',
   emailURL: 'https://hol5104cj2.execute-api.us-east-1.amazonaws.com/dev/email/send',
 };
 
 const prod = {
   apiURL: 'https://3jw0egh7y8.execute-api.us-east-1.amazonaws.com/prod',
-  cloudfrontURL: 'https://d3lgv4qt5prjed.cloudfront.net', // TODO change to prod URL
+  cloudfrontURL: 'https://d3v8t7grqk8z3g.cloudfront.net',
   emailURL: 'https://2dkw1qwaja.execute-api.us-east-1.amazonaws.com/prod/email/send',
 };
 
