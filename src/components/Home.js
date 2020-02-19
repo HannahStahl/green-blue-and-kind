7b9494e7193fb44a03e46bf96e753b1cbfd8507a
Hannah Stahl <hls62@cornell.edu>
2020-02-18 21:22:27 -0600

    Added 1s delay to home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
 import { Waypoint } from 'react-waypoint';
 import DownCarat from './DownCarat';
@@ -8,41 +8,51 @@ const content = require('../content.json');
 
 configureAnchors({ offset: -77 });
 
-const Home = ({ categories }) => (
-  <div className="home-page">
-    <ScrollableAnchor id="home">
-      <Waypoint topOffset="50%">
-        <div className="home-section">
-          <img src={`${process.env.PUBLIC_URL}/home.jpg`} alt="Green, Blue + Kind" className="home-page-image" />
-          <div className="home-text">
-            <h1>Make your mark without leaving a mark.</h1>
-            <a href="/#products" className="shop-products">
-              <p>Shop Products</p>
-              <DownCarat />
-            </a>
+const Home = ({ categories }) => {
+  function showText() {
+    document.getElementById('home-text').className = 'home-text visible';
+  }
+
+  useEffect(() => {
+    setTimeout(showText, 500);
+  }, []);
+
+  return (
+    <div className="home-page">
+      <ScrollableAnchor id="home">
+        <Waypoint topOffset="50%">
+          <div className="home-section">
+            <img src={`${process.env.PUBLIC_URL}/home.jpg`} alt="Green, Blue + Kind" className="home-page-image" />
+            <div id="home-text" className="home-text hidden">
+              <h1>Make your mark without leaving a mark.</h1>
+              <a href="/#products" className="shop-products">
+                <p>Shop Products</p>
+                <DownCarat />
+              </a>
+            </div>
           </div>
-        </div>
-      </Waypoint>
-    </ScrollableAnchor>
-    <ScrollableAnchor id="bio">
-      <Waypoint topOffset="50%">
-        <div className="home-section bio-section">
-          <div className="bio">
-            {content.bio.map((paragraph, index) => (
-              <p key={paragraph} className={index === 0 ? '' : 'bio-paragraph'}>{paragraph}</p>
-            ))}
+        </Waypoint>
+      </ScrollableAnchor>
+      <ScrollableAnchor id="bio">
+        <Waypoint topOffset="50%">
+          <div className="home-section bio-section">
+            <div className="bio">
+              {content.bio.map((paragraph, index) => (
+                <p key={paragraph} className={index === 0 ? '' : 'bio-paragraph'}>{paragraph}</p>
+              ))}
+            </div>
           </div>
-        </div>
-      </Waypoint>
-    </ScrollableAnchor>
-    <ScrollableAnchor id="products">
-      <Waypoint topOffset="50%">
-        <div className="home-section product-categories">
-          <CategoriesList alignment="center" categories={categories || []} />
-        </div>
-      </Waypoint>
-    </ScrollableAnchor>
-  </div>
-);
+        </Waypoint>
+      </ScrollableAnchor>
+      <ScrollableAnchor id="products">
+        <Waypoint topOffset="50%">
+          <div className="home-section product-categories">
+            <CategoriesList alignment="center" categories={categories || []} />
+          </div>
+        </Waypoint>
+      </ScrollableAnchor>
+    </div>
+  );
+};
 
 export default Home;
