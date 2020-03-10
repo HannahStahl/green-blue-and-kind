1075e5837fc571ecbcf45bdeaf997ce910cc112a
Hannah Stahl <hls62@cornell.edu>
2020-03-09 22:41:05 -0500

    Updated prod config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 const dev = {
   apiURL: 'https://dlnum6f7kj.execute-api.us-east-1.amazonaws.com/dev',
-  cloudfrontURL: 'https://d1c360lneb6ftg.cloudfront.net',
+  cloudfrontBaseURL: 'https://d1ljva6zkf6zjh.cloudfront.net',
   emailURL: 'https://hol5104cj2.execute-api.us-east-1.amazonaws.com/dev/email/send',
   userID: 'us-east-1:34ee9094-c95a-4f8f-b2c2-551ef33bd49f',
 };
 
 const prod = {
-  apiURL: 'https://dlnum6f7kj.execute-api.us-east-1.amazonaws.com/dev', // TODO change to prod URL
-  cloudfrontURL: 'https://dd7h6rpqhh194.cloudfront.net',
+  apiURL: 'https://lbe32id9hg.execute-api.us-east-1.amazonaws.com/prod',
+  cloudfrontBaseURL: 'https://d1esxin5o90ebg.cloudfront.net',
   emailURL: 'https://2dkw1qwaja.execute-api.us-east-1.amazonaws.com/prod/email/send',
-  userID: 'us-east-1:34ee9094-c95a-4f8f-b2c2-551ef33bd49f', // TODO change to prod user ID
+  userID: 'us-east-1:0b1dccc0-6c64-4e49-a01a-5670197c6e98',
 };
 
 // Default to dev if not set
@@ -19,6 +19,7 @@ const config = process.env.REACT_APP_STAGE === 'prod'
 
 export default {
   // Add common config values here
+  cloudfrontURL: `${config.cloudfrontBaseURL}/${config.userID}`,
   photosCloudfrontURL: 'https://d3kp4vtpq67hh9.cloudfront.net',
   ...config,
 };
